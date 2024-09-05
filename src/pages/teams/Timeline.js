dadc3b3e0160e669c9589a89c0de67efc2a41925
joyce-tester <sophiaxu1226@gmail.com>
2024-09-05 17:58:08 -0400

    add history

--- src/pages/teams/Timeline.js
+++ src/pages/teams/Timeline.js
@@ -1,44 +1,50 @@
-import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import { items, imageMap } from './items'; // Adjust the path as needed
+import {items, imageMap} from './items.js'
 import { useNavigate } from 'react-router-dom';
 
-export default function Timeline() {
-  const navigate = useNavigate();
 
-  return (
-    <div>
-      <div className="text-3xl text-center font-bold mt-5">Award History</div>
-      <VerticalTimeline lineColor='gray'>
-        {items.map((item, index) => (
-          <VerticalTimelineElement
-            key={index} // Ensure key is unique
-            onTimelineElementClick={() => navigate(item.navigatePath)}
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: 'rgb(245, 167, 66)', color: '#000000' }}
-            date={item.date}
-            iconStyle={{ background: 'rgb(33, 150, 243)' }}
-          > 
-            <h3 className="vertical-timeline-element-title text-[#000080] font-bold mb-6">{item.title}</h3>
-            <h4 className="vtext-2xl font-bold mb-4">{item.location}</h4> 
-            <h1 className="vertical-timeline-element-title">{item.description}</h1>
-            <h5 className="vertical-timeline-element-title">{item.description2}</h5>
+export default function Timeline() {
+    const navigate = useNavigate()
+    
+    return (
+        <div>
+            <div className="text-3xl text-center font-bold mt-5 mb-5">2055 Award History</div>
+            <VerticalTimeline lineColor='gray'>
+                {items.map((item, index) => (
+                    <VerticalTimelineElement
+                    key={index}
+                    onTimelineElementClick={() => navigate(item.navigatePath)}
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: 'rgb(245, 167, 66)', color: '#000000' }}
+                    // contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                    date={item.date}
+                    iconStyle={{ background: 'rgb(33, 150, 243)'}}
+                    // icon={logo}
+                    > 
+                        <h3 className="vertical-timeline-element-title text-white">{item.title}</h3>
+                        <h4 className="vertical-timeline-element-subtitle">{item.description}</h4>
+                        <h4 className="vertical-timeline-element-subtitle">{item.award1}</h4>
+                        <h4 className="vertical-timeline-element-subtitle">{item.award2}</h4>
+                        <h4 className="vertical-timeline-element-subtitle">{item.award3}</h4>
+                        {item.img && <img src={item.img} alt="" className="w-3/4 mt-2 mx-auto border-4"/>}
+
+                        {item.imgKey && imageMap[item.imgKey] && (
+                            <img
+                            src={imageMap[item.imgKey]}
+                            alt={item.title}
+                            className='w-3/4 mt-2 mx-auto border-4'
+                            />
+                        )}
+
+                    </VerticalTimelineElement>
+                ))}
             
-            {item.imgKey && imageMap[item.imgKey] && (
-              <img 
-                src={imageMap[item.imgKey]} 
-                alt={item.title} 
-                className="w-3/4 mt-2 mx-auto border-4"
-              />
-            )}
-          </VerticalTimelineElement>
-        ))}
-        
-        <VerticalTimelineElement
-          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-        />
-      </VerticalTimeline>
-    </div>
-  );
+            <VerticalTimelineElement
+                iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+            />
+
+            </VerticalTimeline>
+        </div>
+    )
 }

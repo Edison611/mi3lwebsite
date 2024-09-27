fba69654d4906ad8492658a2667314ab2b4f1cb3
joyce-tester <joycelin53@gmail.com>
2024-09-27 19:32:11 -0400

    preparing for indexing

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,12 +1,9 @@
-import ShopForm from "./ShopForm";
-
 const OnlineCourses = () => {
+
     return (
-      <div className="max-w-6xl mx-auto p-6 bg-white rounded-lg shadow-md mt-10">
-        <h1 className="text-6xl font-bold text-center mb-5">Online Shop </h1>
-        <p className="text-lg leading-relaxed">We offer many Pet Products as well as VEX Drone parts found below: (Please give it a few seconds to load), Any Issues: Please email mi3lschool@gmail.com</p>
-        <ShopForm />
+      <div className='flex flex-col items-center'>
+          
       </div>
     );
   };
-  export default OnlineCourses;
+export default OnlineCourses;

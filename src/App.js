846c713398c4c719d8c13f331e79fa611fd90730
Edison Ying <edisony611@gmail.com>
2025-07-26 00:09:50 -0400

    Allowed access to course

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ import Userroles from './pages/admin/UserRoles.js';
 import Donation from './pages/shop/donation.js';
 
 function App() {
+  const fullAccess = true; // Change this line for course content access control
   return (
     <div className="flex flex-col min-h-screen">
       
@@ -53,15 +54,32 @@ function App() {
           <Route path="/courses/online/full-vrc-guide" element={<Intro />} />
           <Route path='/courses/online/full-vrc-guide/1' element={<Fvg1 />} />
           <Route path='/courses/online/full-vrc-guide/2' element={<Fvg2 />} />
-          <Route path='/courses/online/full-vrc-guide/3' element={<ProtectedAccessRoute element={<Fvg3 />} page_id={1}/>} />
-          <Route path='/courses/online/full-vrc-guide/4' element={<ProtectedAccessRoute element={<Fvg4 />} page_id={1}/>} />
-          <Route path='/courses/online/full-vrc-guide/5' element={<ProtectedAccessRoute element={<Fvg5 />} page_id={1}/>} />
-          <Route path='/courses/online/full-vrc-guide/6' element={<ProtectedAccessRoute element={<Fvg6 />} page_id={1}/>} />
-          <Route path='/courses/online/full-vrc-guide/7' element={<ProtectedAccessRoute element={<Fvg7 />} page_id={1}/>} />
-          <Route path='/courses/online/full-vrc-guide/8' element={<ProtectedAccessRoute element={<Fvg8 />} page_id={1}/>} />
-          <Route path='/courses/online/full-vrc-guide/9' element={<ProtectedAccessRoute element={<Fvg9 />} page_id={1}/>} />
-          <Route path='/courses/online/full-vrc-guide/10' element={<ProtectedAccessRoute element={<Fvg10 />} page_id={1}/>} />
-          <Route path='/courses/online/full-vrc-guide/11' element={<ProtectedAccessRoute element={<Fvg11 />} page_id={1}/>} />
+          {fullAccess ?
+          <>
+            <Route path='/courses/online/full-vrc-guide/2' element={<Fvg2 />} />
+            <Route path='/courses/online/full-vrc-guide/3' element={<Fvg3 />} />
+            <Route path='/courses/online/full-vrc-guide/4' element={<Fvg4 />} />
+            <Route path='/courses/online/full-vrc-guide/5' element={<Fvg5 />} />
+            <Route path='/courses/online/full-vrc-guide/6' element={<Fvg6 />} />
+            <Route path='/courses/online/full-vrc-guide/7' element={<Fvg7 />} />
+            <Route path='/courses/online/full-vrc-guide/8' element={<Fvg8 />} />
+            <Route path='/courses/online/full-vrc-guide/9' element={<Fvg9 />} />
+            <Route path='/courses/online/full-vrc-guide/10' element={<Fvg10 />} />
+            <Route path='/courses/online/full-vrc-guide/11' element={<Fvg11 />} />)
+          </>
+          :
+          <>
+            <Route path='/courses/online/full-vrc-guide/3' element={<ProtectedAccessRoute element={<Fvg3 />} page_id={1}/>} />
+            <Route path='/courses/online/full-vrc-guide/4' element={<ProtectedAccessRoute element={<Fvg4 />} page_id={1}/>} />
+            <Route path='/courses/online/full-vrc-guide/5' element={<ProtectedAccessRoute element={<Fvg5 />} page_id={1}/>} />
+            <Route path='/courses/online/full-vrc-guide/6' element={<ProtectedAccessRoute element={<Fvg6 />} page_id={1}/>} />
+            <Route path='/courses/online/full-vrc-guide/7' element={<ProtectedAccessRoute element={<Fvg7 />} page_id={1}/>} />
+            <Route path='/courses/online/full-vrc-guide/8' element={<ProtectedAccessRoute element={<Fvg8 />} page_id={1}/>} />
+            <Route path='/courses/online/full-vrc-guide/9' element={<ProtectedAccessRoute element={<Fvg9 />} page_id={1}/>} />
+            <Route path='/courses/online/full-vrc-guide/10' element={<ProtectedAccessRoute element={<Fvg10 />} page_id={1}/>} />
+            <Route path='/courses/online/full-vrc-guide/11' element={<ProtectedAccessRoute element={<Fvg11 />} page_id={1}/>} />
+          </>
+          }
           <Route path='/login' element={<Login/> }/>
           <Route path='/profile' element={<Profile/> }/>
           <Route path='/about' element={<About/> }/>
